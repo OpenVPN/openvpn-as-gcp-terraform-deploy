c1bf61ed7a6c812da213b28fb8f7de43e5d63d84
Yuriy Darnobyt <yura.darnobyt@openvpn.net>
2026-03-26 17:35:11 +0200

    Improve output for AS-Link deployments
    Do not show admin_pw if admin_pw_hash is set
    Use AS Link deployment_name instead IP address
    for host url
    
    Signed-off-by: Yuriy Darnobyt <yura.darnobyt@openvpn.net>

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,21 @@
 locals {
+  has_hash     = length(lookup(var.env_vars, "admin_pw_hash", "")) > 0
   vm_public_ip = google_compute_instance.instance.network_interface[0].access_config[0].nat_ip
+  host_url     = lookup(
+    var.env_vars,
+    "access_server_link.initial_configuration.deployment_name",
+    local.vm_public_ip
+  )
 }
 
 output "site_url" {
   description = "Site Url"
-  value       = "https://${local.vm_public_ip}:443/"
+  value       = "https://${local.host_url}:443/"
 }
 
 output "admin_url" {
   description = "Admin Url"
-  value       = "https://${local.vm_public_ip}:943/admin"
+  value       = "https://${local.host_url}:943/admin"
 }
 
 output "admin_user" {
@@ -19,7 +25,7 @@ output "admin_user" {
 
 output "admin_password" {
   description = "Password for Admin."
-  value       = local.admin_pw
+  value       = local.has_hash ? null : local.admin_pw
   sensitive   = true
 }
 
